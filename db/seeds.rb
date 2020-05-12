126fea48f5fe27219bfc1b30812c28dcc8b134d8
Redfact <tantelyrandria@ymail.com>
2020-05-13 00:59:29 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,42 +10,27 @@
 require 'faker'
 
 User.all.destroy_all
+Event.all.destroy_all
+Participation.all.destroy_all
 
-
-
-10.times do
-  city = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
-end
-
-# Creation de 10 tags
-10.times do
-  tag = Tag.create(title: Faker::Job.field)
-end
-
-
-  
-  # Creation de 10 utilisateurs
-  10.times do
-    user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.safe_email,age: rand(18..60), description: Faker::Hipster.paragraph(sentence_count: 1), city_id:City.ids.sample,password:"123456789")
+  5.times do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create(first_name:first_name,last_name:last_name,email:"#{first_name}-#{last_name}@yopmail.com",password:"123456789",description: Faker::Hipster.paragraph(sentence_count: 3) )
   end
 
-  # Creation de 20 potins
-  20.times do
-
-     gossip = Gossip.create(title: Faker::Book.title,content: Faker::Hipster.paragraph(sentence_count: 10), user_id: User.ids.sample)
-      
-      # Mise à jour des tag_id de tout les gossips
-      gossip.tags << Tag.all.sample
-      
-      # Mise à jour des gossip_id de tout les tags
-      Tag.all.each do |tag|
-        tag.gossips << gossip
-      end
-  end
 
 10.times do
-  comment = Comment.create(user_id: User.ids.sample , 
-    gossip_id: Gossip.ids.sample , 
-    content: Faker::Hipster.paragraph(sentence_count: 3) )
+Event.create(start_date:Faker::Date.between(from: Date.today, to: ("2020-12-31")),
+        duration: rand(1..120),
+        title:Faker::Book.title,
+        description:Faker::Hipster.paragraph(sentence_count: 10),
+        price: rand(1..100),
+        location:Faker::Address.city,
+        admin_id: User.ids.sample )
+end
+
+10.times do
+  Participation.create(user_id: User.ids.sample , event_id: Event.ids.sample )
 end
 
