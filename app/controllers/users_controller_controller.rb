99da247f29834afea437f56dbe5fc3e56f741640
Redfact <tantelyrandria@ymail.com>
2020-05-13 11:20:22 +0200

    add before action user

--- app/controllers/users_controller_controller.rb
+++ app/controllers/users_controller_controller.rb
@@ -1,5 +1,14 @@
 class UsersControllerController < ApplicationController
+	before_action :authenticate_user , only: [:show,:edit]
+
+	def authenticate_user
+		unless user_signed_in?
+			redirect_to new_user_session_path
+		end
+	end
+
   def show
+  	puts "Controller User show"
   end
   	
 
