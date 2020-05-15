7a5b9d04e95f9f0f72c2f23ab4faf6116920b431
Redfact <tantelyrandria@ymail.com>
2020-05-15 18:35:42 +0200

    update active storage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
 
-  resources :users_controller, only: [:show]do 
+  resources :users_controller, only: [:show]do
+  #Permet d'avoir un path qui s'imbrique avec ceux de user
   resources :avatars, only: [:create] 
    end
   resources :events
